815bc4ec07c391c51a20bd6d13e315f36bb880f0
lstenstadvold <lstenstadvold@gmail.com>
2015-03-30 13:58:47 +0200

    Initial commit

---
+++ app/src/androidTest/java/larsstenstadvold/pfi3_2015_lars_s/ApplicationTest.java
@@ -0,0 +1,13 @@
+package larsstenstadvold.pfi3_2015_lars_s;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
