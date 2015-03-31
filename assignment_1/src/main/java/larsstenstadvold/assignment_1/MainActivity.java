c21548e816343b7e54ea6bcffd1f3ee8e08e0c15
lstenstadvold <lstenstadvold@gmail.com>
2015-03-31 13:50:39 +0200

    Added three quotes, translated them to espanish, formatted the views in activity_main.xml, created a button that shows a random quote, prints to log for different statuses (onCreate, onStart osv) /Lars S

--- assignment_1/src/main/java/larsstenstadvold/assignment_1/MainActivity.java
+++ assignment_1/src/main/java/larsstenstadvold/assignment_1/MainActivity.java
@@ -2,16 +2,47 @@ package larsstenstadvold.assignment_1;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 
 public class MainActivity extends ActionBarActivity {
+
+
+    public void nextQuote(View v){
+        TextView displayQuote = (TextView)findViewById(R.id.textView2);
+
+        Random randomGenerator = new Random();
+        int randomInt = randomGenerator.nextInt(3);
+
+        if(randomInt == 0) {
+            String quote1 = getResources().getString(R.string.quote_1);
+            displayQuote.setText(quote1);
+        }
+        if(randomInt == 1){
+            String quote2 = getResources().getString(R.string.quote_2);
+            displayQuote.setText(quote2);
+        }
+        if(randomInt == 2){
+            String quote3 = getResources().getString(R.string.quote_3);
+            displayQuote.setText(quote3);
+        }
+
+        Log.i("MyAPP", "Button pressed");
+        Log.i("RandomInt is: ", String.valueOf(randomInt));
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("MyAPP", "onCreate!");
     }
 
 
@@ -35,5 +66,29 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("MyAPP", "onStop!");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("MyAPP", "onDestroy!");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("MyAPP", "onPause!");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("MyAPP", "onStart!");
     }
 }
