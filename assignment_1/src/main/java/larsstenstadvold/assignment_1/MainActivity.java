a1b94b972cce9f52bed565b223b5169f309fc751
lstenstadvold <lstenstadvold@gmail.com>
2015-03-31 14:29:18 +0200

    Using a StringArray instead of individual strings for quotes. Updated translations. Commented code. /Lars S

--- assignment_1/src/main/java/larsstenstadvold/assignment_1/MainActivity.java
+++ assignment_1/src/main/java/larsstenstadvold/assignment_1/MainActivity.java
@@ -13,13 +13,20 @@ import java.util.Random;
 
 public class MainActivity extends ActionBarActivity {
 
-
+    //method that is called when the next button is pressed
     public void nextQuote(View v){
+        //sets textView2 to the displayQuote
         TextView displayQuote = (TextView)findViewById(R.id.textView2);
-
+        //generates a random rumber between 0-3
         Random randomGenerator = new Random();
-        int randomInt = randomGenerator.nextInt(3);
+        int randomInt = randomGenerator.nextInt(4);
+        //sets quotes_array from the string.xml file to quotesArray
+        String[] quotesArray = getResources().getStringArray(R.array.quotes_array);
+        //sets the text for displayQuote to the quote from a random index in quotesArray
+        displayQuote.setText(quotesArray[randomInt]);
 
+        /*
+        old way of doing it
         if(randomInt == 0) {
             String quote1 = getResources().getString(R.string.quote_1);
             displayQuote.setText(quote1);
@@ -32,6 +39,11 @@ public class MainActivity extends ActionBarActivity {
             String quote3 = getResources().getString(R.string.quote_3);
             displayQuote.setText(quote3);
         }
+        if(randomInt == 3){
+            String quote4 = getResources().getString(R.string.quote_4);
+            displayQuote.setText(quote4);
+        }
+        */
 
         Log.i("MyAPP", "Button pressed");
         Log.i("RandomInt is: ", String.valueOf(randomInt));
